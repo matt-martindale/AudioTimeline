cc7a9e260dfc4f577e3fb47b4e9c4c5419a4c901
Matt Martindale <martindalematt18@yahoo.com>
2020-07-12 15:40:32 -0600

    Initial commit

---
+++ AudioTimeline/AudioTimeline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioTimeline
+//
+//  Created by Matthew Martindale on 7/12/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
