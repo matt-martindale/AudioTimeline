33c1820291590517942490da8504f5d993579cda
Matt Martindale <martindalematt18@yahoo.com>
2020-07-12 15:58:47 -0600

    Added files and storyboard elements

---
+++ AudioTimeline/AudioTimeline/View Controllers/ListenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListenViewController.swift
+//  AudioTimeline
+//
+//  Created by Matthew Martindale on 7/12/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ListenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
